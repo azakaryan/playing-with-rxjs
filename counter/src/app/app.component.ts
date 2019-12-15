892a0312a10cb3d7958f16fa48b7785dd970741d
azakaryan <armenzakary@gmail.com>
2019-12-15 21:52:50 +0100

    fix 1

--- counter/src/app/app.component.ts
+++ counter/src/app/app.component.ts
@@ -9,13 +9,17 @@ import {interval, Subscription} from "rxjs";
 export class AppComponent {
   public count = 0;
   private countSub: Subscription;
+  private isRunning = false;
 
   public onStart(): void {
+    if (this.isRunning) return;
     this.countSub = interval(800).subscribe(val => this.count = val);
+    this.isRunning = true;
   }
 
   public onReset(): void {
     this.countSub.unsubscribe();
     this.count = 0;
+    this.isRunning = false;
   }
 }

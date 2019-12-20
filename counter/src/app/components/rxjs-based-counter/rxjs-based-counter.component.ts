9f273a0affa094d3ae864ce89e5d6556d5aeb1a9
azakaryan <armenzakary@gmail.com>
2019-12-20 07:36:10 +0100

    Added start/stop using stream methododlogy

--- counter/src/app/components/rxjs-based-counter/rxjs-based-counter.component.ts
+++ counter/src/app/components/rxjs-based-counter/rxjs-based-counter.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {fromEvent, merge, NEVER, Observable, Subscription, timer} from "rxjs";
+import {mapTo, switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-rxjs-based-counter',
   templateUrl: './rxjs-based-counter.component.html',
   styleUrls: ['./rxjs-based-counter.component.css']
 })
-export class RxjsBasedCounterComponent implements OnInit {
+export class RxjsBasedCounterComponent implements OnInit, OnDestroy {
+  @ViewChild('startBtn', {static: true, read: ElementRef}) startBtn :ElementRef;
+  @ViewChild('stopBtn', {static: true, read: ElementRef}) stopBtn :ElementRef;
+  public counter: number = 0;
+  private tickSpeed = 800;
+  private btnStart$: Observable<any>;
+  private btnStop$: Observable<any>;
+  private counter$: Observable<number>;
+  private counterSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.init();
+
+    this.counter$ = this.mergeStreams()
+      .pipe(
+        switchMap((isTicking: boolean) =>
+          isTicking
+            ? timer(0, this.tickSpeed)
+            : NEVER)
+      );
+
+    this.counterSubscription = this.counter$
+      .subscribe(
+        (value: number) => this.counter = value
+      );
   }
 
+  ngOnDestroy(): void {
+    this.counterSubscription.unsubscribe();
+  }
+
+  /*
+  * Private Helpers
+  * */
+  private init(): void {
+    this.btnStart$ = fromEvent(this.startBtn.nativeElement, 'click');
+    this.btnStop$ = fromEvent(this.stopBtn.nativeElement, 'click');
+  }
+
+  // Merge streams and convert to boolean
+  private mergeStreams(): Observable<boolean> {
+    return merge(
+      this.btnStart$.pipe(mapTo(true)),
+      this.btnStop$.pipe(mapTo(false)),
+    )
+  }
 }
